0572f1cb0c5dc037f112ff482904d101fa839fda
perompak_kode <iqbaltech29@gmail.com>
2024-03-11 18:33:45 +0700

    learn searchbar and searchview

---
+++ app/src/main/java/com/jimbonlemu/fundamental_android/MenuActivity.kt
@@ -0,0 +1,25 @@
+package com.jimbonlemu.fundamental_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.jimbonlemu.fundamental_android.databinding.ActivityMenuBinding
+
+class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView.editText.setOnEditorActionListener { textView, actionId, keyEvent ->
+                searchBar.setText(searchView.text)
+                searchView.hide()
+                Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                false
+            }
+        }
+    }
+}
