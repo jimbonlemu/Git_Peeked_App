a9eecb9863387a66f130aef9ef4e942268553886
perompak_kode <iqbaltech29@gmail.com>
2024-03-29 05:24:46 +0700

    Fixed issue FavoriteActivity where list wasn't updating when deleting the last favorite user. Now, the adapter updates even when the list is empty.

--- app/src/main/java/com/jimbonlemu/fundamental_android/view/pages/FavoriteActivity.kt
+++ app/src/main/java/com/jimbonlemu/fundamental_android/view/pages/FavoriteActivity.kt
@@ -5,53 +5,51 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jimbonlemu.fundamental_android.data.local.entity.FavoriteEntity
 import com.jimbonlemu.fundamental_android.data.remote.response.UserItem
 import com.jimbonlemu.fundamental_android.databinding.ActivityFavoriteBinding
 import com.jimbonlemu.fundamental_android.view.adapter.ListGithubUserAdapter
 import com.jimbonlemu.fundamental_android.view.view_model.FavoriteViewModel
 import com.jimbonlemu.fundamental_android.view.view_model_factory.FavoriteViewModelFactory
-
 class FavoriteActivity : AppBarActivity("Favorites Page") {
 
     private lateinit var binding: ActivityFavoriteBinding
+    private lateinit var adapter: ListGithubUserAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFavoriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initRecylcerView()
+        initRecyclerView()
 
         val favViewModelFactory = FavoriteViewModelFactory.getInstance(this)
         val favViewModel: FavoriteViewModel by viewModels {
             favViewModelFactory
         }
 
-        favViewModel.getListAllFavorite().observe(this){value ->
-            if (value.isNullOrEmpty()){
+        favViewModel.getListAllFavorite().observe(this) { value ->
+            setupAdapter(value)
+            if (value.isNullOrEmpty()) {
                 Toast.makeText(this, "You don\'t favorite any users at all", Toast.LENGTH_SHORT).show()
-            }else{
-            val listFavorites = arrayListOf<UserItem>()
-                value.map { data ->
-                    val item = UserItem(login = data.username, avatarUrl = data.userImage)
-                    listFavorites.add(item)
-                }
-                setupAdapter(listFavorites)
             }
         }
+
     }
 
-    private fun initRecylcerView(){
-        with(binding.rvFavorite){
-            val layoutManager = LinearLayoutManager(this@FavoriteActivity)
-            this.layoutManager = layoutManager
-            val itemDecor = DividerItemDecoration(this@FavoriteActivity, layoutManager.orientation)
-            addItemDecoration(itemDecor)
+    private fun initRecyclerView() {
+        binding.rvFavorite.layoutManager = LinearLayoutManager(this)
+        binding.rvFavorite.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+    }
+
+    private fun setupAdapter(listFavorites: List<FavoriteEntity>) {
+        val userItems = listFavorites.map { favoriteEntity ->
+            UserItem(login = favoriteEntity.username, avatarUrl = favoriteEntity.userImage)
         }
-    }
 
-    private fun setupAdapter(listFavorites:ArrayList<UserItem>){
-        val adapter = ListGithubUserAdapter()
-        adapter.submitList(listFavorites)
-        binding.rvFavorite.adapter = adapter
+        if (!::adapter.isInitialized) {
+            adapter = ListGithubUserAdapter()
+            binding.rvFavorite.adapter = adapter
+        }
+        adapter.submitList(userItems)
     }
 }
