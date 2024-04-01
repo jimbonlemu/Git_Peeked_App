c2c0c3d9bff00db6e5f00757b2cc4d7c49ccc989
perompak_kode <iqbaltech29@gmail.com>
2024-04-02 06:39:27 +0700

    changes library method calling.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,28 +1,27 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
     id("com.google.devtools.ksp")
     id("kotlin-parcelize")
+
 }
 
 android {
-    namespace = "com.jimbonlemu.fundamental_android"
+    namespace = "com.jimbonlemu.git_peeked"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.jimbonlemu.fundamental_android"
+        applicationId = "com.jimbonlemu.git_peeked"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 31
-        versionName = "2.0"
+        targetSdk = 33
+        versionCode = 1
+        versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        buildConfigField("String", "BASE_URL", "\"${project.findProperty("BASE_URL")}\"")
-        buildConfigField("String", "AUTH_TOKEN", "\"${project.findProperty("AUTH_TOKEN")}\"")
+       
     }
 
-    buildFeatures {
+    buildFeatures{
         viewBinding = true
         buildConfig = true
     }
@@ -46,40 +45,40 @@ android {
 }
 
 dependencies {
-//     Default Library
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-//    Data Store and Database Library
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+//  Default Library
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
-//    Live Data Library
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+//  Data Store and Database Library
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
-//    Component Library
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("com.facebook.shimmer:shimmer:0.5.0")
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+//  Live Data Library
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
 
-//    API Library
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+//  Component Library
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.circleimageview)
+    implementation(libs.shimmer)
+    implementation(libs.androidx.viewpager2)
+    implementation(libs.glide)
 
-//    Testing Library
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//  API Library
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
 }
